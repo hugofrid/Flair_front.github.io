38d55a442df2591508fcf139870bdbd0fae76664
Laura Chammas <laurachammas@macbook-air-de-laura-3.home>
2020-01-08 11:18:46 +0100

    accueil logo et description

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './flair.png';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      </header>
+
+      <body>
+
+      <h1>Bienvenue sur Flair</h1>
+      <h3>Flair est une application qui vous permet de savoir où investir en île de France.
+Nous nous basons sur des données publiques telles que les évolutions de tendance des quartiers, les futurs aménagements...</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          C'est parti !
         </a>
-      </header>
+
+        <button
+                  className="App-link"
+                  onClick="window.open('https://reactjs.org')">C'est parti !</button>
+
+      </body>
+      <footer>
+      <p>
+          Edit <code>src/App.js</code> and save to reload.
+      </p>
+      </footer>
     </div>
   );
 }
