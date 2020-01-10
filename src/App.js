c1f426e9026dd9060665df4c317f08cc77d23db1
Laura Chammas <laurachammas@macbook-air-de-laura-3.home>
2020-01-10 02:38:28 +0100

    mergeissue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import logo from './flair.png';
-=======
 import React, {Component} from 'react';
 import Map from './containers/map/Map.js';
->>>>>>> Stashed changes
 import './App.css';
 import logo from './flair.png';
 import Accueil from './containers/accueil/Accueil.js';
@@ -14,36 +9,6 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 function App() {
 
   return (
-<<<<<<< Updated upstream
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
-      <body>
-
-      <h1>Bienvenue sur Flair</h1>
-      <h3>Flair est une application qui vous permet de savoir où investir en île de France.
-Nous nous basons sur des données publiques telles que les évolutions de tendance des quartiers, les futurs aménagements...</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          C'est parti !
-        </a>
-
-        <button
-                  className="App-link"
-                  onClick="window.open('https://reactjs.org')">C'est parti !</button>
-
-      </body>
-      <footer>
-      <p>
-          Edit <code>src/App.js</code> and save to reload.
-      </p>
-=======
 
     <div className="app">
 
@@ -60,10 +25,10 @@ Nous nous basons sur des données publiques telles que les évolutions de tendan
         <div className="credits">
         Credits @ Bienassis, Botrel, Chammas, Fridlansky, Lemerle, Noé & Nunez
         </div>
->>>>>>> Stashed changes
       </footer>
     </div>
   );
 }
 
 export default App;
+
