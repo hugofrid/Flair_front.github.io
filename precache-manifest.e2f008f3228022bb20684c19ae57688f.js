c5eaaaa4693c8edb919f8fe2ec047170f579cb35
hugofrid <hugofrid2@gmail.com>
2020-01-28 15:47:01 +0100

    Updates

---
+++ precache-manifest.e2f008f3228022bb20684c19ae57688f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e5802ab859b2e513d9d488b6347c0f",
+    "url": "/Flair_front/index.html"
+  },
+  {
+    "revision": "6b8c6a18ef191436aa35",
+    "url": "/Flair_front/static/css/2.156fe21f.chunk.css"
+  },
+  {
+    "revision": "1dc83623123cf4e54248",
+    "url": "/Flair_front/static/css/main.5c59674f.chunk.css"
+  },
+  {
+    "revision": "6b8c6a18ef191436aa35",
+    "url": "/Flair_front/static/js/2.833c4de0.chunk.js"
+  },
+  {
+    "revision": "a3bb8f51c96cc0b29bbdf0de685bd34e",
+    "url": "/Flair_front/static/js/2.833c4de0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dc83623123cf4e54248",
+    "url": "/Flair_front/static/js/main.d3451ae4.chunk.js"
+  },
+  {
+    "revision": "14b28099d48305d71f61",
+    "url": "/Flair_front/static/js/runtime-main.d399e565.js"
+  },
+  {
+    "revision": "7ccd174f4a28323dce8b9e5cc70b01f1",
+    "url": "/Flair_front/static/media/flair.7ccd174f.png"
+  },
+  {
+    "revision": "2c0c8c7d49a09f93025a4f195a23fc81",
+    "url": "/Flair_front/static/media/help.2c0c8c7d.png"
+  },
+  {
+    "revision": "71c806428f9d8c76f8dd491ee177382c",
+    "url": "/Flair_front/static/media/marker.71c80642.png"
+  },
+  {
+    "revision": "e1349f50bd69f5c60cc37859b8026ed1",
+    "url": "/Flair_front/static/media/search.e1349f50.png"
+  },
+  {
+    "revision": "25bac1f8cc09c705956822bee6fd1426",
+    "url": "/Flair_front/static/media/selectedMarker.25bac1f8.png"
+  }
+]);
