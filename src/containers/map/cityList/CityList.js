45c1ebd717f03340e1339bd07bf2eb401a632b9e
hugofrid <hugofrid2@gmail.com>
2020-01-30 15:24:06 +0100

    prep integration

--- src/containers/map/cityList/CityList.js
+++ src/containers/map/cityList/CityList.js
@@ -5,10 +5,14 @@ import IconBtn from '../../../componants/iconBtn/iconBtn.js';
 import { closeIcon, listIcon, sortDscIcon, sortAscIcon } from '../../../icons/icons.js'
 import JaugeBar from '../../../componants/jaugeBar/jaugeBar';
 import SearchInput from '../../../componants/searchInput/SearchInput.js'
-import { colors } from '../mapStyle'
+import { colors, colorBoundaries } from '../mapStyle'
 
 
 function CityList(props) {
+
+    const [loadLimite, setLoadLimite] = useState(100);
+    const [orderList, setOrderList] = useState(false);
+
 
     const sortByCityName = (a, b) => {
         if (a.properties.city_name < b.properties.city_name)
@@ -19,27 +23,27 @@ function CityList(props) {
     }
 
     const sortByDisplayedInfo = (a, b) => {
-      
-       if(orderList === "-") {
+        if (orderList) {
             if (parseFloat(a.properties[props.displayedInfo]) < parseFloat(b.properties[props.displayedInfo]))
-            return -1;
-        if (parseFloat(a.properties[props.displayedInfo]) > parseFloat(b.properties[props.displayedInfo]))
-               return 1;
-       }
-       else {
-        if (parseFloat(a.properties[props.displayedInfo]) > parseFloat(b.properties[props.displayedInfo]))
-        return -1;
-    if (parseFloat(a.properties[props.displayedInfo]) < parseFloat(b.properties[props.displayedInfo]))
-        return 1;}
+                return -1;
+            if (parseFloat(a.properties[props.displayedInfo]) > parseFloat(b.properties[props.displayedInfo]))
+                return 1;
+        }
+        else {
+            if (parseFloat(a.properties[props.displayedInfo]) > parseFloat(b.properties[props.displayedInfo]))
+                return -1;
+            if (parseFloat(a.properties[props.displayedInfo]) < parseFloat(b.properties[props.displayedInfo]))
+                return 1;
+        }
 
-    } 
-  
+    }
 
-    const sortFeatures =  (featureList) => {
-        if (featureList && featureList.length > 0) {
+
+    const sortFeatures = (filteredList) => {
+        if (filteredList && filteredList.length > 0) {
             let list = [];
 
-            list = featureList.sort(sortByCityName)
+            list = filteredList.sort(sortByCityName)
             list = list.sort(sortByDisplayedInfo);
             return list;
         }
@@ -47,14 +51,8 @@ function CityList(props) {
     }
 
 
-    const [loadLimite, setLoadLimite] = useState(100);
-    const [orderList, setOrderList] = useState("+");
-
-
-    useEffect(() => { setFilteredList(sortFeatures(filteredList)) }, [orderList])
-
-    const [featureList, setFeatureList] = useState(sortFeatures(props.features));
-    const [filteredList, setFilteredList] = useState(featureList);
+    const [featureList, setFeatureList] = useState(props.features);
+    const [filteredList, setFilteredList] = useState([...sortFeatures(featureList)]);
 
 
 
@@ -62,82 +60,87 @@ function CityList(props) {
         props.setShowCityList(!props.showCityList);
     }
 
-    const getColor = (a,b) => {
-        if(a=="estimation2"){
-            if(b<=0){
-                return colors[0];
-            }
-            if(b>0 && b<=6){
-                return colors[1];
-            }
-            if(b>6 && b<=11){
-                return colors[2];
-            }
-            if(b>11){
-                return colors[3];
-            }
-            
-        }else{
-            if(b<=0){
-                return colors[0];
-            }
-            if(b>0 && b<=6){
-                return colors[1];
-            }
-            if(b>6 && b<=11){
-                return colors[2];
-            }
-            if(b>11){
-                return colors[3];
-            }
+    const getColor = (estimation, value) => {
+        if (value <= colorBoundaries(estimation)[1]) {
+            return colors[0];
         }
-        
-    }
+        if (value > colorBoundaries(estimation)[1] && value <= colorBoundaries(estimation)[2]) {
 
-    const renderedColors = colors.reduce((acc, obj) =>
-        acc = acc + "," + obj)
+            return colors[1];
+        }
+        if (value > colorBoundaries(estimation)[2] && value <= colorBoundaries(estimation)[3]) {
+
+            return colors[2];
+        }
+        if (value > colorBoundaries(estimation)[3]) {
+
+            return colors[3];
+        }
+
+    }
+    useEffect(() => {
+        console.log(filteredList[0].properties.estimation2)
+        
+    },[filteredList])
     
+
+    useEffect(() => {
+        console.log(orderList)
+        const orderedFilterList = sortFeatures(filteredList)
+       // console.log(filteredList[0].properties.estimation2)
+        setFilteredList([...orderedFilterList]);
+        //console.log(filteredList[0].properties.estimation2)
+    },[orderList])
+
+    // function changeOrder(order){
+    //     console.log("avant",order,orderList, filteredList[0].properties);
+    //           
+    //     console.log("apres",order,orderList, filteredList[0].properties);
+
+    // }
+
     return (
         <div className={"listComponent " + (props.showCityList ? "isOpen" : "")}>
 
             <IconBtn className={("toggleIcon ") + (props.showCityList && "closeIcon")} icon={props.showCityList ? closeIcon : listIcon} onClick={reduceList}></IconBtn>
 
 
-            {props.showCityList && featureList && featureList.length >0 && loadLimite &&
+            {props.showCityList && featureList && featureList.length > 0 && loadLimite &&
                 <div className="visibleList">
-                
-                    <IconBtn className="listOrder" icon={orderList === "+" ? sortDscIcon : sortAscIcon} onClick={() => orderList === "+" ? setOrderList("-") : setOrderList("+")}></IconBtn>
+
+                    <IconBtn className="listOrder" icon={orderList ? sortDscIcon : sortAscIcon} onClick={() => setOrderList(!orderList)}></IconBtn>
 
                     <SearchInput arraySource={featureList} returnedArray={(elem) => setFilteredList(elem)}></SearchInput>
 
 
                     <div className="list">
-                        {
+                        <div className="scrollComponant">
+                            {filteredList && 
 
-                            filteredList.slice(0, loadLimite).map((elem, index) =>
+                                filteredList.slice(0, loadLimite).map((elem, index) =>
 
-                                <div className={"city " + ((elem.properties === props.activeFeature) ? "selected" : "")} key={index}
-                                    onClick={() => {
-                                        props.onClickFeature(elem, index);
+                                    <div className={"city " + ((elem.properties === props.activeFeature) ? "selected" : "")} key={index}
+                                        onClick={() => {
+                                            props.onClickFeature(elem, index);
 
-                                    }}
+                                        }}
 
-                                >
-                                    <div className="cityName">  {capitalize(elem.properties.city_name)}  - {elem.properties.codePostal}</div>
-                                    <div className="infobyCity">
-                                        { (props.displayedInfo=="estimation2") &&
-                                            <p>Prix du m² à 2 ans : <span style={{color:getColor(props.displayedInfo,elem.properties[props.displayedInfo])}}>{elem.properties[props.displayedInfo] > 0 ? "+"+elem.properties[props.displayedInfo]:elem.properties[props.displayedInfo]}%</span></p>
-                                        }
-                                        { (props.displayedInfo=="estimation5") &&
-                                            <p>Prix du m² à 5 ans : <span style={{color:getColor(props.displayedInfo,elem.properties[props.displayedInfo])}}>{elem.properties[props.displayedInfo] > 0 ? "+"+elem.properties[props.displayedInfo]:elem.properties[props.displayedInfo]}%</span></p>
-                                        }
+                                    >
+                                        <div className="cityName">  {capitalize(elem.properties.city_name)}  - {elem.properties.codePostal}</div>
+                                        <div className="infoByCity">
 
-                                        {/* <JaugeBar colors={renderedColors} point={elem.properties[props.displayedInfo]} ></JaugeBar> */}
-                                    </div>
-                                </div>)
+                                            <div className="estimation">{props.displayedInfo === "estimation2" ? 'Prix du m² à 2 ans :' : 'Prix du m² à 5 ans :'}</div><div className="pourcent" style={{ color: getColor(props.displayedInfo, elem.properties[props.displayedInfo]) }}>{elem.properties[props.displayedInfo] > 0 ? "+" + (elem.properties[props.displayedInfo]).toFixed(2) : (elem.properties[props.displayedInfo]).toFixed(2) }%</div>
 
-                        }{(loadLimite <= filteredList.length) &&
-                            <div className="showMore" onClick={() => setLoadLimite(loadLimite + 100)}>voir plus </div>}
+
+
+
+
+                                        </div>
+                                    </div>)
+
+                            }{(loadLimite <= filteredList.length) &&
+                                <div className="showMore" onClick={() => setLoadLimite(loadLimite + 100)}>voir plus </div>}
+                        </div>
                     </div>
                 </div>
             }
